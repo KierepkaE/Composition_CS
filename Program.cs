64d130cc0d00a1e2efed0d178d0717d9a39131a8
kierepkae <kierepkaedyta@gmail.com>
2019-11-30 13:27:32 +0000

    create instances of Installer, Logger

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ namespace Composition {
 
   class Program {
     static void Main (string[] args) {
-
+      var dbMigrator = new DbMigrator (newLogger ());
+      var logger = new Logger ();
+      var installer = new Installer (logger);
+      dbMigrator.Migrate ();
+      installer.INSTALL ();
     };
   }
 
